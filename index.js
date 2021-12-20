d2e323d184c0dfe384ac98c377a95304acca0835
dafid palevi <43629045+dafidpalevi1@users.noreply.github.com>
2021-12-20 23:02:07 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+        alert("Hai Beb Nopi 😘")
+        alert("Aku Kangen Banget Sama Kamu 😘")
+        alert("Semoga Kamu Nggak Bosan Ya Nungguin Aku 😘")
+        alert("Kamu Sudah Makan Belum Beb Nopi 😘")
+        alert("Sekarang Lagi Ngapain Beb Nopi 😘")
+
